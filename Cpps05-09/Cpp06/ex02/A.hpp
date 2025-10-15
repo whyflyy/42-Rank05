04c22e14092a53afd0ecc6b6e673c854edd437df
Whyflyy <cavadasribeiro.joao@gmail.com>
2025-10-15 16:43:30 +0100

    Finished Cpp06, need to fix some final touches

---
+++ Cpps05-09/Cpp06/ex02/A.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.hpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 13:43:20 by jcavadas          #+#    #+#             */
+/*   Updated: 2025/10/15 13:48:18 by jcavadas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+/// COLOURS ///
+#define RESET		"\033[0m"
+#define RED			"\033[38;5;160m"
+#define GREEN		"\033[38;5;76m"
+#define BLUE		"\033[38;5;111m"
+#define YELLOW		"\033[38;5;220m"
+#define MAGENTA		"\033[38;5;183m"
+#define CYAN    	"\033[38;5;87m"
+#define ORANGE 		"\033[38;5;208m"
+#define TURQUOISE	"\033[38;5;45m"
+#define PEACH		"\033[38;5;217m"
+
+#include "Base.hpp"
+
+class A : public Base
+{
+	private:
+	
+	public:
+	
+};
---
+++ Cpps05-09/Cpp06/ex02/main.cpp
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 13:53:38 by jcavadas          #+#    #+#             */
+/*   Updated: 2025/10/15 16:38:55 by jcavadas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+#define SEPARATOR "\033[38;5;208m\n==============================\n\033[0m"
+
+Base*	generate(void)
+{
+	int random = std::rand() % 3;
+	
+	switch (random)
+	{
+	case 0:
+		return (new A);
+		break;
+	case 1:
+		return (new B);
+		break;
+	case 2:
+		return (new C);
+		break;
+	}
+	return (NULL);
+}
+
+void	identify(Base* p)
+{
+	if (dynamic_cast<A*>(p) != NULL)
+		std::cout << YELLOW << "The object's type is " << TURQUOISE << "A" << YELLOW << "!" << RESET << std::endl;
+	else if (dynamic_cast<B*>(p) != NULL)
+		std::cout << YELLOW << "The object's type is " << TURQUOISE << "B" << YELLOW << "!" << RESET << std::endl;
+	else if (dynamic_cast<C*>(p) != NULL)
+		std::cout << YELLOW << "The object's type is " << TURQUOISE << "C" << YELLOW << "!" << RESET << std::endl;
+	else
+		std::cout << RED << "Something went wrong with the object's type!" << RESET << std::endl;	
+}
+
+void	identify(Base& p)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << PEACH << "The object's type is " << CYAN << "A" << PEACH << "!" << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		try
+		{
+			(void)dynamic_cast<B&>(p);
+			std::cout << PEACH << "The object's type is " << CYAN << "B" << PEACH << "!" << RESET << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			try
+			{
+				(void)dynamic_cast<C&>(p);
+				std::cout << PEACH << "The object's type is " << CYAN << "C" << PEACH << "!" << RESET << std::endl;
+			}
+			catch(const std::exception& e)
+			{
+						std::cout << RED << "Something went wrong with the object's type!" << RESET << std::endl;
+			}
+		}
+	}
+}
+
+int	main()
+{
+	Base	*base;
+	std::srand(static_cast<unsigned>(time(NULL)));
+	
+	base = generate();
+	if (base == NULL)
+		return 0;
+
+	Base	*Brah;
+	Brah = generate();
+	if (Brah == NULL)
+		return 0;
+
+	std::cout << SEPARATOR << MAGENTA << "Identify with Pointer" << SEPARATOR << RESET << std::endl;
+	identify(base);
+	std::cout << std::endl;
+	identify(Brah);
+
+	
+	std::cout << SEPARATOR << MAGENTA << "Identify with Reference" << SEPARATOR << RESET << std::endl;
+	identify(*base);
+	std::cout << std::endl;
+	identify(*Brah);
+
+}
